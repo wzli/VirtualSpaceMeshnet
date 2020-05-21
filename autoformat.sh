1603f8377717e5416808d4dd103d72fbf9935c10
WenZheng Li <me@wenzheng.li>
2020-05-22 02:12:32 +0900

    first commit

---
+++ autoformat.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+find . -type f -name '*.h' -o -iname '*.c' -o -iname '*.cpp' -o -iname '*.hpp' \
+    | grep -v -e build/ -e submodules/ \
+    | xargs clang-format --verbose -style=file -i -fallback-style=none

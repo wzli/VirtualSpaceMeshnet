736340ed462b42a25aef61598e798dc6608aae24
WenZheng Li <me@wenzheng.li>
2020-05-30 19:24:00 +0900

    forgot to send out timestamp

--- include/vsm/time_sync.hpp
+++ include/vsm/time_sync.hpp
@@ -19,6 +19,7 @@ public:
 
     Duration getTime() const { return _local_time() + _offset_estimate; }
     Duration getLocalTime() const { return _local_time(); }
+    Duration getOffset() const { return _offset_estimate; }
 
     Duration fromLocalTime(Duration time) const { return time + _offset_estimate; }
     Duration toLocalTime(Duration time) const { return time - _offset_estimate; }
